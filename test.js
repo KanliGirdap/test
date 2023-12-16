4ef0978139d9fc0f430142eacc9a4facbdd891c7
KanliGirdap <54188238+KanliGirdap@users.noreply.github.com>
2023-12-16 18:18:09 +0300

    Update test.js

--- test.js
+++ test.js
@@ -3,14 +3,15 @@ function start(){
 let trysize=0,x=setInterval(function(){let e=document.querySelector('a[href="#user-details"]');if(console.log("Searching...",e.href),100==trysize&&clearInterval(x),e){clearInterval(x);let t=document.querySelector("#user-details > div > div"),l=document.createElement("p"),r=document.querySelector("#username"),i=document.querySelector("#password"),d=localStorage.getItem(r.value),a=localStorage.getItem("urller");(void 0==a||"undefined"==a)&&(a="URL Bulunamadı"),a=a.split(",");let n="";d&&"undefined"!=d&&void 0!=d?n=d:(d=localStorage.getItem("lastID")||0,localStorage.setItem("lastID",Number(d)+1),"undefined"!=(n=a[Number(d)])&&void 0!=n&&n||(localStorage.setItem("lastID","0"),n=a[Number(d)])),(void 0==n||"undefined"==n)&&(localStorage.setItem("lastID","0"),n=a[0]),"URL Bulunamadı"!==a[0]&&localStorage.setItem(r.value,n),l.id="extraarea",l.innerHTML=`<b>URL:</b> <a href="${n}">${n}</a> <br>
 <br>
 UYDU CİHAZLARI İÇİN
-<br>
-URL: wooqjuz.780139.shop<br>
+<br><br>
+URL: ${n}<br>
 Kullanıcı Adı: 4032zgrr<br>
 Şifre: BuCMET3cE8
 <br><br><br>
 
 XTREAM OLARAK LİSTE : 
-<br>
+<br><br>
+URL: ${n}<br>
 <b>Kullanıcı Adı:</b> ${r.value}<br>
 <b>Şifre:</b> ${i.value}<br>
 <br><br><br>
